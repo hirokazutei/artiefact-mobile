6010c1ccca540ac444f23903044bb67742242c4e
Hirokazu Tei <hirokazutei@nextdoor.com>
2020-04-21 08:45:32 -0400

    FEAT: Adjusted Icon component and storybook

--- src/components/atoms/Icon/__storybook__/index.stories.tsx
+++ src/components/atoms/Icon/__storybook__/index.stories.tsx
@@ -3,57 +3,85 @@ import * as React from "react";
 import { storiesOf } from "@storybook/react-native";
 import { select, withKnobs } from "@storybook/addon-knobs";
 import Provider from "../../../../../storybook/Provider";
-import { AllColorKeys } from "../../../../symbols";
-import Icon, { Props, IconSizes, IconTypes } from "../";
+import { selectAllColor } from "../../../../../storybook/knobs";
+import Icon, { IconProps } from "../";
+import { IconSizeKey, IconTypeKey } from "../const";
 
-const DEFAULT_PROPS: Props = {
-  name: "camera"
+const DEFAULT_PROPS: IconProps = {
+  name: "camera",
 };
 
-const typeSelect: { [key in IconTypes]: IconTypes } = {
+const typeSelect: { [key in IconTypeKey]: IconTypeKey } = {
   camera: "camera",
   successCircle: "successCircle",
   errorCircle: "errorCircle",
   warningCircle: "warningCircle",
-  noConnection: "noConnection"
+  noConnection: "noConnection",
 };
 
-const colorSelect: { [key in AllColorKeys]?: AllColorKeys } = {
-  primary: "primary",
-  secondary: "secondary",
-  tertiary: "tertiary"
-};
-
-const sizeSelect: { [key in IconSizes]: IconSizes } = {
+const sizeSelect: { [key in IconSizeKey]: IconSizeKey } = {
   tiny: "tiny",
   small: "small",
   medium: "medium",
   large: "large",
   huge: "huge",
   massive: "massive",
-  macro: "macro"
+  macro: "macro",
 };
 
-const getRequiredProps = (overrides = {}): Props => {
+const getRequiredProps = (overrides: Partial<IconProps> = {}): IconProps => {
   const { name } = {
     ...DEFAULT_PROPS,
-    ...overrides
+    ...overrides,
   };
   return {
-    name: select("Icon Type", typeSelect, name)
+    name: select("Icon Type", typeSelect, name),
   };
 };
 
-const getOptionalProps = () => {
+const getOptionalProps = (
+  overrides: Partial<IconProps> = {}
+): Partial<IconProps> => {
+  const { size, color } = overrides;
   return {
-    color: select("Color", colorSelect, "primary" as AllColorKeys),
-    size: select("Size", sizeSelect, "medium")
+    color: select("Color", selectAllColor, color),
+    size: select("Size", sizeSelect, size),
   };
 };
 
 storiesOf("Atoms/Icon")
   .addDecorator((story: () => React.ReactElement) => <Provider story={story} />)
   .addDecorator(withKnobs)
-  .add("default", () => (
+  .add("Default", () => (
     <Icon {...getRequiredProps()} {...getOptionalProps()} />
+  ))
+  .add("Error", () => (
+    <Icon
+      {...getRequiredProps({ name: "errorCircle" })}
+      {...getOptionalProps()}
+    />
+  ))
+  .add("No Connection", () => (
+    <Icon
+      {...getRequiredProps({ name: "noConnection" })}
+      {...getOptionalProps()}
+    />
+  ))
+  .add("Success", () => (
+    <Icon
+      {...getRequiredProps({ name: "successCircle" })}
+      {...getOptionalProps()}
+    />
+  ))
+  .add("Warning", () => (
+    <Icon
+      {...getRequiredProps({ name: "warningCircle" })}
+      {...getOptionalProps()}
+    />
+  ))
+  .add("Tiny", () => (
+    <Icon {...getRequiredProps()} {...getOptionalProps({ size: "tiny" })} />
+  ))
+  .add("Macro", () => (
+    <Icon {...getRequiredProps()} {...getOptionalProps({ size: "macro" })} />
   ));
--- src/components/atoms/Icon/index.tsx
+++ src/components/atoms/Icon/index.tsx
@@ -1,70 +1,33 @@
 import React from "react";
 import RNVIcon from "react-native-vector-icons/Feather";
-import { allColors, AllColorKeys } from "../../../symbols";
+import { allColors, AllColorKey } from "../../../symbols";
+import { IconTypeKey, IconSizeKey, iconSizes, iconTypes } from "./const";
 
-export type IconTypes =
-  | "camera"
-  | "errorCircle"
-  | "noConnection"
-  | "successCircle"
-  | "warningCircle";
-type FeatherIconNames =
-  | "camera"
-  | "check-circle"
-  | "x-circle"
-  | "alert-circle"
-  | "wifi-off";
-
-const iconTypes: Readonly<{ [key in IconTypes]: FeatherIconNames }> = {
-  camera: "camera",
-  successCircle: "check-circle",
-  errorCircle: "x-circle",
-  warningCircle: "alert-circle",
-  noConnection: "wifi-off"
-};
-
-export type IconSizes =
-  | "tiny"
-  | "small"
-  | "medium"
-  | "large"
-  | "huge"
-  | "massive"
-  | "macro";
-
-const iconSizes: Readonly<{ [key in IconSizes]: number }> = {
-  tiny: 12,
-  small: 16,
-  medium: 24,
-  large: 32,
-  huge: 56,
-  massive: 72,
-  macro: 104
-};
-
-export type Props = {
-  name: IconTypes;
-  size?: IconSizes;
-  color?: AllColorKeys;
+type IconProps = {
+  name: IconTypeKey;
+  size?: IconSizeKey;
+  color?: AllColorKey;
 };
 
 /**
  * Icon
  *
  * @param props - properties
+ *
+ * Required:
  * @param props.name - name of Icon
- * @param props.size - size of Icon
- * @param props.color - color of Icon
+ *
+ * Optional:
+ * @param [props.size] - size of Icon
+ * @param [props.color] - color of Icon
  */
-const Icon: React.FC<Props> = ({
-  size = "medium",
-  color = "primary",
-  name
-}: Props): React.ReactElement => {
+const Icon = ({ size = "medium", color = "primary", name }: IconProps) => {
   const iconSize = iconSizes[size];
   const iconColor = allColors[color];
   const iconName = iconTypes[name];
   return <RNVIcon name={iconName} size={iconSize} color={iconColor} />;
 };
+
+export { IconProps };
 
 export default Icon;
