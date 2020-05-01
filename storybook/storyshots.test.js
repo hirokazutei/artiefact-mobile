3692630fa277f1fb4797fcf12fde910b23a75a16
Hirokazu Tei <hirokazutei@nextdoor.com>
2020-05-01 10:15:22 -0400

    FEAT: Added storyshot testing

---
+++ storybook/storyshots.test.js
@@ -0,0 +1,12 @@
+import initStoryshots, {
+  multiSnapshotWithOptions,
+  Stories2SnapsConverter,
+} from "@storybook/addon-storyshots";
+
+initStoryshots({
+  configPath: "storybook",
+  stories2snapsConverter: new Stories2SnapsConverter({
+    snapshotsDirName: "./",
+  }),
+  test: multiSnapshotWithOptions({}),
+});
