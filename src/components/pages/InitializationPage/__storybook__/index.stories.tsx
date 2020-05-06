752625f2718ac3d409c4dc3d671cde28e2909672
Hirokazu Tei <hirokazutei@nextdoor.com>
2020-05-06 09:44:54 -0400

    CHORE: Generated storyshots

--- src/components/pages/InitializationPage/__storybook__/index.stories.tsx
+++ src/components/pages/InitializationPage/__storybook__/index.stories.tsx
@@ -3,12 +3,12 @@ import * as React from "react";
 import { storiesOf } from "@storybook/react-native";
 import { withKnobs } from "@storybook/addon-knobs";
 import Provider from "../../../../../storybook/Provider";
-import { navigationMockProp } from "../../../../../storybook/mockProps";
-import InitializationPage from "../index";
+// Because the page has side effects, use the Template instead.
+import InitializationPage from "../../../templates/LoadingTemplate";
 
 storiesOf("Pages/InitializationPage", module)
   .addDecorator((story: () => React.ReactElement<null>) => (
     <Provider story={story} fullPage={true} />
   ))
   .addDecorator(withKnobs)
-  .add("Default", () => <InitializationPage navigation={navigationMockProp} />);
+  .add("Default", () => <InitializationPage />);
